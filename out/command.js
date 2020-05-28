e7e51df3bf0b7414094bec6ca1a3f2540587c8be
ddehilster <!Xoxota69>
2020-05-28 17:17:29 -0400

    VSCODE-NLP-007 syntax enhanced, added out dir, fixed some command stuff
    Signed-off-by: ddehilster <dehilster@gmail.com>

---
+++ out/command.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+class NLPCommands {
+    constructor(ctx) {
+        this._ctx = ctx;
+        ctx.subscriptions.push(vscode.commands.registerCommand("nlp.analyze", this.analyze));
+    }
+    static attach(ctx) {
+        if (!exports.nlpCommands) {
+            exports.nlpCommands = new NLPCommands(ctx);
+        }
+        return exports.nlpCommands;
+    }
+    analyze() {
+        console.log(`NLP Analyzing!!!`);
+        const testFolder = "${workspaceFolder}";
+        const fs = require('fs');
+        fs.readdir(testFolder, (err, files) => {
+            files.forEach(file => {
+                console.log(file);
+            });
+        });
+    }
+}
+exports.NLPCommands = NLPCommands;
+//# sourceMappingURL=command.js.map
